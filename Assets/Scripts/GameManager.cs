782bfd486904ee0ad664a62908e7a130febb1562
Bratie Fanut <bratiefanut@gmail.com>
2022-02-12 19:29:33 +0200

    Menu and scene transitions
    - created a menu scene and implemented scene transitions

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public string playerName;
+
+    public int bestScore;
+    public string bestPlayerName;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/UIhandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIhandler : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
